3f8ae24daca9b7f4defaa967650a0b6e19181086
Matheus <msgoncalves@inf.ufpel.edu.br>
2025-01-01 06:55:08 -0300

    Criando páginas para os novos documentos

---
+++ app/itbi-pedro-osorio/page.tsx
@@ -0,0 +1,17 @@
+export default function ITBIPedroOsorio() {
+  return (
+    <div className="grid align-top">
+      <div className="grid justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-2 sm:items-start">
+          <div className="flex flex-col gap-1">
+            <h2 className="font-semibold text-2xl">ITBI (Pedro Osório)</h2>
+
+            <div className="flex gap-2 items-center text-neutral-500">
+              <p>Em construção...</p>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
