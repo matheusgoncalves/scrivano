20a1bcb739aaef17756e80da178ce3eb0cb6d803
Matheus <msgoncalves@inf.ufpel.edu.br>
2025-01-01 06:27:00 -0300

    Criando lista de documentos

---
+++ data/models.ts
@@ -0,0 +1,6 @@
+export const models = [
+    { id: 1, name: 'ITBI (Pedro Osório)', src: '/images/lorem-ipsum.jpg', alt: 'itbi de pedro osorio', href: '/itbi-pedro-osorio' },
+    { id: 2, name: 'Declaração de Comprador', src: '/images/lorem-ipsum.jpg', alt: 'declaração de comprador', href: '/declaracao-comprador' },
+    { id: 3, name: 'Declaração de Vendedor', src: '/images/lorem-ipsum.jpg', alt: 'declaração de vendedor', href: '/declaracao-vendedor' },
+  ];
+  
