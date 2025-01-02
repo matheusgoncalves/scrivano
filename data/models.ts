6c2ee4344e01ef22e107463092b4c32afcd95c69
Matheus <msgoncalves@inf.ufpel.edu.br>
2025-01-01 21:22:19 -0300

    Adicionando modelo de documento e imagens para cada um deles

--- data/models.ts
+++ data/models.ts
@@ -1,6 +1,6 @@
 export const models = [
-    { id: 1, name: 'ITBI (Pedro Osório)', src: '/images/lorem-ipsum.jpg', alt: 'ITBI de Pedro Osório', href: '/itbi-pedro-osorio' },
-    { id: 2, name: 'Declaração do(a) comprador(a)', src: '/images/lorem-ipsum.jpg', alt: 'Declaração do(a) comprador(a)', href: '/declaracao-comprador' },
-    { id: 3, name: 'Declaração do(a) vendedor(a)', src: '/images/lorem-ipsum.jpg', alt: 'Declaração do(a) vendedor(a)', href: '/declaracao-vendedor' },
-  ];
-  
+  { id: 1, name: 'ITBI (Pedro Osório)', src: '/images/preview-itbi-cerrito.png', alt: 'ITBI de Pedro Osório', href: '/itbi-pedro-osorio' },
+  { id: 2, name: 'ITBI (Cerrito)', src: '/images/preview-itbi-pedro-osorio.png', alt: 'ITBI de Cerrito', href: '/itbi-cerrito' },
+  { id: 3, name: 'Declaração do(a) vendedor(a)', src: '/images/preview-declaracao-vendedor.png', alt: 'Declaração do(a) vendedor(a)', href: '/declaracao-vendedor' },
+  { id: 4, name: 'Declaração do(a) comprador(a)', src: '/images/preview-declaracao-comprador.png', alt: 'Declaração do(a) comprador(a)', href: '/declaracao-comprador' },
+];
